1680a9b9e3b4c61c1b3442177447c6389a66c64c
gaius_omega <gaius7071@gmail.com>
2025-08-04 00:12:44 +0000

    Changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
 from .models import Book
 from .models import Library
+from django.views.generic import DetailView
 
 # Create your views here.
 def book_list(request):
